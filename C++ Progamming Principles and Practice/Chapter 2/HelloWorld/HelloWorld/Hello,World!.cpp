82841b0acbce5c90a111e27ceb2138473be211ca
bruce kellerman <bruce kellerman>
2016-08-11 20:49:35 -0400

    initial commit, finished chapter 2 / hello world of C++ Principles and practice

---
+++ C++ Progamming Principles and Practice/Chapter 2/HelloWorld/HelloWorld/Hello,World!.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+	cout << "Hello World!\n"; // output "Hello World!"
+	cout << "Here we go!\n";
+	keep_window_open();
+	return 0;
+}
