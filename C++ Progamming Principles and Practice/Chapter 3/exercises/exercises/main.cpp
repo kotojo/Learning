bbca2e4562b8a7c7c1f99d2bedb3bd33f0a406b4
bruce kellerman <brucek17@hotmail.com>
2016-08-18 22:41:40 -0400

    finished chapter 3 exercises

--- C++ Progamming Principles and Practice/Chapter 3/exercises/exercises/main.cpp
+++ C++ Progamming Principles and Practice/Chapter 3/exercises/exercises/main.cpp
@@ -110,6 +110,77 @@ int main() {
 	}
 
 	// Exercise 8
+	cout << "Enter an integer please.\n";
+	cin >> val1;
+	cout << "The number " << val1;
+	if (val1 % 2 == 0) {
+		cout << " is an even number.\n";
+	}
+	else {
+		cout << " is an odd number.\n";
+	}
+
+	// Exercise 9
+	cout << "Enter a number between 0 and 4 (inclusive) and I will spell it out for you.\n";
+	cin >> val1;
+	if (val1 == 0) {
+		cout << "Zero.\n";
+	}
+	else if (val1 == 1) {
+		cout << "One.\n";
+	}
+	else if (val1 == 2) {
+		cout << "Two.\n";
+	}
+	else if (val1 == 3) {
+		cout << "Three.\n";
+	}
+	else if (val1 == 4) {
+		cout << "Four.\n";
+	}
+	else {
+		cout << "Not a number I know, sorry.\n";
+	}
+
+	// Excersize 10
+	cout << "Enter a math operator followed by two integers.\n";
+	string operation;
+	cin >> operation >> val1 >> val2;
+	if (operation == "+") {
+		cout << val1 << " + " << val2 << " = " << val1 + val2;
+	}
+	else if (operation == "-") {
+		cout << val1 << " - " << val2 << " = " << val1 - val2;
+	}
+	else if (operation == "*") {
+		cout << val1 << " * " << val2 << " = " << val1 * val2;
+	}
+	else if (operation == "/") {
+		cout << val1 << " / " << val2 << " = " << val1 / val2;
+	}
+	cout << ".\n";
+
+	// Exercise 11
+	cout << "How many pennies do you have?\n";
+	cin >> val1;
+	cout << "How many nickels do you have?\n";
+	cin >> val2;
+	cout << "How many dimes do you have?\n";
+	cin >> val3;
+	cout << "How many quarters do you have?\n";
+	int val4;
+	cin >> val4;
+	cout << "How many half dollars do you have?\n";
+	int val5;
+	cin >> val5;
+
+	cout << "You have " << val1 << " pennies.\n";
+	cout << "You have " << val2 << " nickels.\n";
+	cout << "You have " << val3 << " dimes.\n";
+	cout << "You have " << val4 << " quarters.\n";
+	cout << "You have " << val5 << " half dollars.\n";
+	int sum = val1 + val2 * 5 + val3 * 10 + val4 * 25 + val5 * 50;
+	cout << "The value of all of your coins is $" << floor(sum / 100) << "." << sum % 100 <<".\n";
 
 	keep_window_open();
 }
