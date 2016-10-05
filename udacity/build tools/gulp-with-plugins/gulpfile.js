6c39ddd2514ec965196da7f3eebd94303d6abff3
bruce kellerman <brucek17@hotmail.com>
2016-10-04 20:54:54 -0400

    finished forms lesson, halfway through build tools

---
+++ udacity/build tools/gulp-with-plugins/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+gulp.task('default', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	browserSync.init({
+		server: './'
+	});
+	browserSync.stream();
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+});
